471060d93430fb616bea7a1938c9becf4db04863
Wlasovets <wlasovets@gmail.com>
2016-12-13 21:44:13 +0300

    13.12.16

--- www/js/main.js
+++ www/js/main.js
@@ -1,11 +1,11 @@
 /**
- *
  * @param int itemId - ID товара
  * @return - в случае успеха обновятся данные корзины на странице
  */
 function addToCart(itemId) {
 
     console.log("js - addToCart()");
+
     $.ajax({
         type: 'POST',
         async: false,
@@ -19,5 +19,29 @@ function addToCart(itemId) {
                 $('#removeCart_' + itemId).show();
             }
         }
-    })
+    });
+}
+
+/**
+ * @param int itemId - ID товара
+ * @return - в случае успеха обновятся данные корзины на странице
+ */
+function removeFromCart(itemId) {
+
+    console.log("js - removeFromCart("+ itemId +")");
+
+    $.ajax({
+        type: 'POST',
+        async: false,
+        url: "/cart/removefromcart/" + itemId + '/',
+        dataType: 'json',
+        success: function (data) {
+
+            if (data['success']) {
+                $('#cartCntItems').html(data['cntItems']);
+                $('#addCart_' + itemId).show();
+                $('#removeCart_' + itemId).hide();
+            }
+        }
+    });
 }
