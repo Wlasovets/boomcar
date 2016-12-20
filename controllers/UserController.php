8db5acfd57a92c1bcee9fb392c032e3ecc5e1dde
Wlasovets <wlasovets@gmail.com>
2016-12-20 19:32:42 +0300

    20.12.2016

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -3,3 +3,29 @@
  * Контроллер функций пользователя
  *
  */
+
+// Подключаем модели
+include_once '../models/UsersModel.php';
+
+/**
+ * AJAX регистрация пользователя
+ * Инициализация сессионной переменной ($_SESSION['user'])
+ *
+ * @return json - массив данных нового пользователя
+ */
+function registerAction()
+{
+    $email = isset($_REQUEST['email']) ? $_REQUEST['email'] : null;
+    $email = trim($email);
+
+    $pwd1 = isset($_REQUEST['pwd1']) ? $_REQUEST['pwd1'] : null;
+    $pwd2 = isset($_REQUEST['pwd2']) ? $_REQUEST['pwd2'] : null;
+
+    $phone = isset($_REQUEST['phone']) ? $_REQUEST['phone'] : null;
+    $address = isset($_REQUEST['address']) ? $_REQUEST['address'] : null;
+    $name = isset($_REQUEST['name']) ? $_REQUEST['name'] : null;
+    $name = trim($name);
+
+    $resData = null;
+    $resData = checkRegisterParams($email, $pwd1, $pwd2);
+}
--- models/UsersModel.php
+++ models/UsersModel.php
@@ -4,3 +4,82 @@
  *
  */
 
+/**
+ * @param string $email - почта
+ * @param string $pwdMD5 - пароль зашифрованный в MD5
+ * @param string $name - имя пользователя
+ * @param string $phone - телефон
+ * @param string $address - адрес пользователя
+ * @return array|bool|resource - массив данных нового пользователя
+ */
+function registerNewUser($email, $pwdMD5, $name, $phone, $address)
+{
+
+    $email = htmlspecialchars(mysql_real_escape_string($email));
+    $name = htmlspecialchars(mysql_real_escape_string($name));
+    $phone = htmlspecialchars(mysql_real_escape_string($phone));
+    $address = htmlspecialchars(mysql_real_escape_string($address));
+
+    $sql = "INSERT INTO
+            users(`email`, `pwd`, `name`, `phone`, `address`)
+            VALUES ('{$email}', '{$pwdMD5}', '{$name}', '{$phone}', '{$address}')";
+
+    $rs = mysql_query($sql);
+
+    if ($rs) {
+
+        $sql = "SELECT *
+                FROM users
+                WHERE (`email` = '{$email}', `pwd` = '{$pwdMD5}')
+                LIMIT 1";
+
+        $rs = mysql_query($sql);
+        $rs = createSmartyRsArray($rs);
+
+        if (isset($rs[0])) {
+            $rs['success'] = 1;
+        } else {
+            $rs['success'] = 0;
+        }
+
+    } else {
+        $rs['success'] = 0;
+    }
+
+    return $rs;
+}
+
+/**
+ * Проверка параметров для регистрации пользователя
+ *
+ * @param string $email - email
+ * @param string $pwd1 - пароль
+ * @param string $pwd2 - повтор пароля
+ * @return array - результат
+ */
+function checkRegisterParams($email, $pwd1, $pwd2)
+{
+    $res = null;
+
+    if (!$email) {
+        $res['success'] = false;
+        $res['message'] = 'Введите email';
+    }
+
+    if (!$pwd1) {
+        $res['success'] = false;
+        $res['message'] = 'Введите пароль';
+    }
+
+    if (!$pwd2) {
+        $res['success'] = false;
+        $res['message'] = 'Введите повтор пароля';
+    }
+
+    if ($pwd1 != $pwd2) {
+        $res['success'] = false;
+        $res['message'] = 'Пароли не совпадают';
+    }
+
+    return $res;
+}
