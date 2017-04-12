2d30f9bb62b0e6de8d1538c01d7f647ef3c9aba0
Wlasovets <wlasovets@gmail.com>
2017-04-12 17:12:38 +0200

    12.04.2017

--- models/OrdersModel.php
+++ models/OrdersModel.php
@@ -11,5 +11,32 @@
  */
 function makeNewOrder($name, $phone, $address)
 {
+    //> Инициализация переменных
+    $userId = $_SESSION['user']['id'];
+    $name = htmlspecialchars(mysql_real_escape_string($name));
+    $phone = htmlspecialchars(mysql_real_escape_string($phone));
+    $address = htmlspecialchars(mysql_real_escape_string($address));
 
+    $comment = "id пользователя: {$userId}<br />
+                Имя: {$name}<br />
+                Телефон: {$phone}<br />
+                Адрес: {$address}";
+
+    $dateCreated = date('Y.m.d H:m:s');
+    $userIp = $_SERVER['REMOTE_ADDR'];
+    //<
+
+    // формирование запроса к БД
+    $sql = "INSERT INTO
+            orders (`user_id`, `date_created`, `date_payment`, `status`, `comment`, `user_ip`)
+            VALUES ('{$userId}', '{$dateCreated}', null, '0', '{$comment}', '{$userIp}')";
+
+    $rs = mysql_query($sql);
+
+    if($rs) {
+        $orderId = mysql_insert_id();
+        return $orderId;
+    }
+
+    return false;
 }
